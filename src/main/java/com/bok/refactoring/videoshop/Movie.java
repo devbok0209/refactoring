0a74c5c1ce445e6704abcf1eae8bafdf5458c88d
devbok0209 <devbok0209@bodycodi.com>
2022-10-24 01:14:04 +0900

    [CP1] 앞으로의 변경사항을 고려해, getCharge, getFrequentRenterPoints 메서드 Movie 클래스로 이동

--- src/main/java/com/bok/refactoring/videoshop/Movie.java
+++ src/main/java/com/bok/refactoring/videoshop/Movie.java
@@ -14,4 +14,36 @@ public class Movie {
 
     private String title;
     private int priceCode;
+
+    public double getCharge(int daysRented) {
+        double thisAmount = 0;
+
+        switch (getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented >  2) {
+                    thisAmount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount +=daysRented * 3;
+                break;
+            case Movie.CHILDREN:
+                thisAmount += 1.5;
+                if (daysRented > 3) {
+                    thisAmount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if ((getPriceCode()) == Movie.NEW_RELEASE
+                && daysRented > 1 ) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
