f83070aee263efc4ba93812448e7a31bfedcb48c
devbok0209 <devbok0209@bodycodi.com>
2022-10-24 01:48:22 +0900

    [CP1] getFrequentRenterPoints() Price.class 이동

--- src/main/java/com/bok/refactoring/videoshop/price/Price.java
+++ src/main/java/com/bok/refactoring/videoshop/price/Price.java
@@ -1,8 +1,13 @@
 package com.bok.refactoring.videoshop.price;
 
 
+
 public abstract class Price {
     abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
+
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
 }
